048c7ac8a34f9458e55b64dcee6c8c80c9f840e5
Данил <danil.beloborodoff@yandex.ru>
2021-05-25 00:07:49 +0300

    Готов к защите

--- source/js/script.js
+++ source/js/script.js
@@ -5,13 +5,27 @@ closeButton.onclick = function () {
   closeButton.classList.toggle('header__toggle--closed');
 }
 
-let beforeAndAfterImg = document.querySelector('.before-and-after__img');
+let purposeCheckbox = document.querySelectorAll('.purpose__checkbox');
+document.addEventListener("DOMContentLoaded", () => {
+  closeButton.style.display = "block";
+  closeButton.classList.remove('header__toggle--closed');
+  menu.classList.add('header__nav--closed');
+});
+
 let beforeAndAfterImgButton = document.querySelector('.toggle-wrapper__toggle-button');
-beforeAndAfterImgButton.onchange = function () {
-  if (beforeAndAfterImgButton.checked) {
-    beforeAndAfterImg.src = "img/cat_after_program1x.png";
+let counter = 0;
+beforeAndAfterImgButton.addEventListener("click", () => {
+let catImgBefore = document.querySelector(".cat-range__img--first");
+let catImgAfter = document.querySelector(".cat-range__img--second");
+counter++;
+  beforeAndAfterImgButton.classList.toggle('toggle-wrapper__toggle-button--checked');
+  if (counter % 2 !== 0) {
+    catImgAfter.classList.remove("cat-range__img--hidden");
+    catImgBefore.classList.add("cat-range__img--hidden");
+  } else {
+    catImgAfter.classList.add("cat-range__img--hidden");
+    catImgBefore.classList.remove("cat-range__img--hidden");
   }
-  else {
-    beforeAndAfterImg.src = "img/cat_before_program1x.png";
-  }
-}
+});
+
+
